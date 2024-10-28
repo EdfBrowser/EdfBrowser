0849aef51a3a42a8512efc333f3559fe0b2e0da6
pusong <user0gao@gmail.com>
2024-10-28 12:30:31 +0800

    Add project files.

---
+++ EdfBrowser.App/Form1.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace EdfBrowser.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
